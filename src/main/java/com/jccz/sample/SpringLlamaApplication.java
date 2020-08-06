f2ee689ee453f02d80a02a530dee3c6d317183de
juanczapata <jcarczapata@gmail.com>
2020-08-05 20:40:46 -0500

    First commit

---
+++ src/main/java/com/jccz/sample/SpringLlamaApplication.java
@@ -0,0 +1,13 @@
+package com.jccz.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLlamaApplication.class, args);
+	}
+
+}
